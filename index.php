018729612b29e9d6ae8710939498419c0b2ed52f
solenn <vosport46@gmail.com>
2020-05-30 23:17:16 +0200

    debut projet

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Patisserie Madlenn</title>
+        <link rel="stylesheet" media="screen" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    </head>
+    <head>
+        <div id="entete">
+            <img src="img/logo.png" alt="logoMadlenn" name="logoMadlenn">
+            <div id="titre">
+                <h1> Patisserie Madlenn</h1>
+                <h2> Saint-Malo </h2>
+            </div>
+            <div></div>
+        </div>
+        <div id="menu">
+            <a href="#">Nos produits</a>
+            <a href="#">Notre Maison</a>
+            <a href="#">Nos adresses</a>
+            <a href="#">Contactez-Nous</a>
+        </div>
+
+    </head>
+    <body>
+
+
+    </body>
+</html>

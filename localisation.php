a7bf716fd5d6e11b63c90a5f674882faf7ebd782
solenn <vosport46@gmail.com>
2020-06-19 23:59:40 +0200

    localisation page fini

--- localisation.php
+++ localisation.php
@@ -1,3 +1,42 @@
 <?php
 include('header.php');
 ?>
+    <h2 class="hpadding"> Nous Retrouvez </h2>
+    <section id="pageLocalisation">
+        <div class="emplacementBoutique">
+            <div class="pictureLocalisation"> <img src="img/boutique.jpg" alt="Photo boutique patisserie" name="photoBoutique"></div>
+            <div class="descriptionLocation">
+                <h3> La boutique Madlenn </h3>
+                <p> 23 Avenue des fontenelles 35400 Saint-Malo</p>
+                <p>Horaires: tous les jours 8h-13h et 16h-19h, le dimanche 8h-13h </p>
+                <a  href="https://www.google.fr/maps/place/23+Avenue+des+Fontenelles,+35400+Saint-Malo/@48.6684153,-1.981902,17z/data=!3m1!4b1!4m5!3m4!1s0x480e82313f11fda7:0xcf617d143af3c2a0!8m2!3d48.6684153!4d-1.9797133"><img src="img/location.png" alt="logo de localisation" name="logo de localisation">Plan </a>
+            </div>
+        </div>
+        <div class="emplacementBoutique">
+            <div class="pictureLocalisation"> <img src="img/marche.jpg" alt="Photo stand Marché" name="photoMarche"></div>
+            <div class="descriptionLocation">
+                <h3> Marché de Saint-Servan </h3>
+                <p> Jour : Mardi et Vendredi</p>
+                <a href="https://www.google.fr/maps/place/3+Place+Bouvet,+35400+Saint-Malo/@48.6377961,-2.0193843,17z/data=!3m1!4b1!4m5!3m4!1s0x480e813f17157371:0x5e357949377a84ff!8m2!3d48.6377961!4d-2.0171956"><img src="img/location.png" alt="logo de localisation" name="logo de localisation">Plan</a>
+            </div>
+        </div>
+        <div class="emplacementBoutique">
+            <div class="pictureLocalisation"> <img src="img/marche.jpg" alt="Photo stand Marché" name="photoMarche"></div>
+            <div class="descriptionLocation">
+                <h3> Marché de Paramé </h3>
+                <p> Jour : Mercredi et Samedi</p>
+                <a href="https://www.google.fr/maps/place/1+Avenue+de+la+Borderie,+35400+Saint-Malo/@48.6589805,-1.9841398,17z/data=!3m1!4b1!4m5!3m4!1s0x480e83cd99ac300f:0x917fedba13a4a01e!8m2!3d48.6589805!4d-1.9819511"><img src="img/location.png" alt="logo de localisation" name="logo de localisation">Plan</a>
+            </div>
+        </div>
+        <div class="emplacementBoutique">
+            <div class="pictureLocalisation"> <img src="img/marche.jpg" alt="Photo stand Marché" name="photoMarche"></div>
+            <div class="descriptionLocation">
+                <h3> Marché de Rocabey  </h3>
+                <p> Jour : Jeudi et Samedi </p>
+                <a href="https://www.google.fr/maps/place/Rue+des+Halles,+35400+Saint-Malo/@48.6522419,-2.006715,17z/data=!3m1!4b1!4m5!3m4!1s0x480e817aa56c04b5:0xf5dc375df8d64263!8m2!3d48.6522419!4d-2.0045263"> <img src="img/location.png" alt="logo de localisation" name="logo de localisation">Plan</a>
+            </div>
+        </div>
+    </section>
+<?php
+include('footer.php');
+?>
